a03eeeed9aba5eaa27eab10b18495b17fc0bb888
Steve Ross <steve@ross.wtf>
2024-07-10 08:07:03 +0000

    Update file s3-cleanup.sh

--- s3-cleanup.sh
+++ s3-cleanup.sh
@@ -3,12 +3,12 @@
 # modified from https://stackoverflow.com/a/74823523
 
 S3CMD=/usr/bin/s3cmd
-RETENTION=29
 
-while getopts s: flag
+while getopts s:r: flag
 do
   case "${flag}" in
     s) server=${OPTARG};;
+    r) retention=${OPTARG};;
   esac
 done
 
@@ -18,13 +18,18 @@ then
   exit 1
 fi
 
+if [ -z ${retention+x} ];
+then
+  retention=30
+fi
+
 echo "cleaning up old backups on $server s3 bucket"
 
 $S3CMD ls --recursive s3://wp-$server-daily-backups | while read -r line;
   do
     createDate=`echo $line|awk {'print $1" "$2'}`
     createDate=`date -d"$createDate" +%s`
-    olderThan=`date --date "$RETENTION days ago" +%s`
+    olderThan=`date --date "$retention days ago" +%s`
 
     if [[ $createDate -lt $olderThan ]]
       then
